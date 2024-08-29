4435ecd052ff5788fc3ed48bb22b4dc0ee7205a4
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-29 16:00:24 +0200

    fix: folders for setup

--- init_onyxia.sh
+++ init_onyxia.sh
@@ -3,14 +3,13 @@
 DEFI=$1
 
 WORK_DIR=/home/onyxia/work
-CLONE_DIR=${WORK_DIR}/repo-git
 
 # Clone repository
 REPO_URL=https://github.com/datagouv/odu-notebooks.git
-git clone --depth 1 $REPO_URL $CLONE_DIR
+git clone --depth 1 $REPO_URL $WORK_DIR
 
 # Install additional packages from requirements.txt
-REQUIREMENTS_FILE=${CLONE_DIR}/requirements.txt
+REQUIREMENTS_FILE=${WORK_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
 
 # Open the relevant notebook when starting Jupyter Lab
