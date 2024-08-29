b9f77d16554007f1077841b70db6f6a0d9156da1
Pierlou <pierlou.ramade@data.gouv.fr>
2024-08-29 16:47:13 +0200

    feat: remove all files except the requested notebook on launch

--- init_onyxia.sh
+++ init_onyxia.sh
@@ -8,9 +8,14 @@ WORK_DIR=/home/onyxia/work
 REPO_URL=https://github.com/datagouv/odu-notebooks.git
 git clone --depth 1 $REPO_URL $WORK_DIR
 
+
+
 # Install additional packages from requirements.txt
 REQUIREMENTS_FILE=${WORK_DIR}/requirements.txt
 [ -f $REQUIREMENTS_FILE ] && pip install -r $REQUIREMENTS_FILE
 
-# Open the relevant notebook when starting Jupyter Lab
+# Delete everything that is not the requested notebook
+find $WORK_DIR -type f ! -name "${LABEL}.ipynb" -exec rm -f {} +
+
+# Open the notebook when starting Jupyter Lab
 echo "c.LabApp.default_url = '/lab/tree/${DEFI}.ipynb'" >> /home/onyxia/.jupyter/jupyter_server_config.py
